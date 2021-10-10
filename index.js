2990ce07fb6b8df6838615521959bbb6e636b79b
SamsAgency <samstore637@gmail.com>
2021-10-10 16:53:09 +0300

    removed config from git ignore

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ connection();
 
 const privateKey = process.env.PRIVATE_KEY 
 
-if (!privateKey) {
+if (!config.get("jwtPrivateKey")) {
 		throw new Error('FATAL ERROR!!: jwtPrivateKey is not defined')
 	}
 
